457fdeacf53b45eacf1f75e65d8d814c0cd024c2
EnesArslan8 <enes8arslan@gmail.com>
2023-09-22 10:19:48 +0300

    Brands component created

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Caption from './components/Caption';
+import Brands from './components/Brands';
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Caption/>
+        <Brands/>
     </div>
   );
 }
---
+++ src/components/Brands.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Brands = () => {
+  return (
+    <div className='width'>
+        
+    </div>
+  )
+}
+
+export default Brands
